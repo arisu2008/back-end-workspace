0f0103fcd5e314739918abe7e5276e27a7c32773
arisu2008 <arisu201054@gmail.com>
2024-07-08 21:08:56 +0900

    240708-2

--- 2_java/08_Polymorphism/src/com/kh/array/practice1/Application.java
+++ 2_java/08_Polymorphism/src/com/kh/array/practice1/Application.java
@@ -7,6 +7,7 @@ import com.kh.array.practice1.model.Student;
 public class Application {
 
 	public static void main(String[] args) {
+		
 		Student[] arr = new Student[5];
 		
 		arr[0] = new Student("박성철", "자바", 100);
@@ -15,17 +16,23 @@ public class Application {
 		arr[3] = new Student("김경주", "화면", 98);
 		arr[4] = new Student("김현호", "자바", 95);
 		
-		System.out.println();
+		System.out.println(Arrays.toString(arr));
 		
 		for(int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
 		
+		System.out.println();
+		
+		for(Student student : arr) {
+			System.out.println(student);
+		}
+		
 	}
-	
-	System.out.println();
-	for(Student student : arr) {
-		System.out.println(student);
-	}
-	
+
 }
+
+
+
+
+
